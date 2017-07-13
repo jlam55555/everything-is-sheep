6dca28ceb6290233467f18d26a37e1fa873de7f2
Jonathan Lam <jlam55555@gmail.com>
2017-07-13 14:26:07 -0400

    Added some content and formatting

---
+++ posts/on-trees.md
@@ -0,0 +1,9 @@
+Trees are living things. They are organisms that have parts called (as people usually call them) trunks, leaves, branches, and roots.
+
+The trunks of trees have many layers. One, the one for growing, is only one cell thick, which is really weird. The center of the trees is called the heartwood.
+
+Heartwood is dead wood.
+
+There are also many types of trees, including spruce, pine, oak, and more. Fortunately, there are a lot of trees, since we humans need to breathe oxygen in order to survive. Trees provide oxygen.
+
+Unfortunately, trees are being killed. Funny, how one of the things that we need are the things that we keep killing. Trees getting killed has been part of an enormous chain of events, including Global Warming, which has been killing off even more things, including species of stuff which have eventually, or at least *will* eventually affect us.
--- server.js
+++ server.js
@@ -15,29 +15,63 @@ app.set("view engine", "handlebars");
 // get name of all posts
 var postList = [];
 fs.readdir("./posts", function(error, posts) {
-  for(var i = 0; i < posts.length; i++) {
-    if(posts[i].slice(-5) === ".json") {
-      postList.push({filename: posts[i].slice(0, -5), title: require("./posts/" + posts[i]).title});
+  for(var post of posts) {
+    if(post.slice(-5) === ".json") {
+      var postData = require("./posts/" + post);
+      postList.push({filename: post.slice(0, -5), title: postData.title, data: postData});
     }
+  }
+  postList.sort(function(post1, post2) {
+    return new Date(post2.data.date) - new Date(post1.data.date);
+  });
+});
+
+// get name of all authors
+var authorList = [];
+fs.readdir("./authors", function(error, authors) {
+  for(var author of authors) {
+    var authorData = require("./authors/" + author);
+    authorList.push({filename: author.slice(0, -5), name: authorData.name});
   }
 });
 
 // handle routing (index)
 app.get("/", function(req, res) {
-  res.render("index", {postList: postList});
+  res.render("index", {postList: postList, authorList: authorList});
 });
 
 // handle routing (posts)
 app.get("/posts/*", function(req, res) {
-  var postData = require("./" + req.url + ".json");
+  var postData = require("." + req.url.replace(/\%20| /g, "-") + ".json");
+  postData.postList = postList;
+  postData.authorList = authorList;
+  
   fs.readFile("./" + req.url + ".md", "utf-8", function(error, markdown) {
     if(error) {
       return console.log(error);
     }
-    postData["markdown"] = markdown.replace(/\n/g, "\\n");
+    postData.markdown = markdown.replace(/\n/g, "\\n");
     res.render("post", postData);
   });
 });
+
+// handle routing (authors)
+app.get("/authors/*", function(req, res) {
+  var authorData = require("." + req.url.replace(/\%20| /g, "-").toLowerCase() + ".json");
+  authorData.postList = postList;
+  authorData.authorList = authorList;
+  var authoredPosts = []
+  for(var post of postList) {
+    if(post.data.author == authorData.name) {
+      authoredPosts.push(post);
+    }
+  }
+  authorData.authoredPosts = authoredPosts;
+  res.render("author", authorData);
+});
+
+// handle routing (resources)
+app.use("/res", express.static("res"));
 
 // listen on port
 app.listen(process.env.PORT || 5000);
