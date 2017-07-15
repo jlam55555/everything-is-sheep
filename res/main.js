8dea32f1d1e713b3b763daaf7dd53d895770fb9e
Jonathan Lam <jlam55555@gmail.com>
2017-07-15 14:29:24 -0400

    Added some modifications for web app

--- res/main.js
+++ res/main.js
@@ -1,4 +1,16 @@
 $(function() {
+
+  // make web app standalone (i.e., not open links in safari)
+  // src: https://gist.github.com/irae/1042167 (condensed version)
+  (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
+
+  // add extra top section on web app
+  if(("standalone" in navigator) && navigator["standalone"]) {
+    $("#content").addClass("webApp");
+    $("#sidebar").addClass("webApp"); 
+  }
+
+  // mobile menu
   var menuButton = $("#menuButton");
   var sidebar = $("#sidebar");
   var searchBar = $("#searchBar");
