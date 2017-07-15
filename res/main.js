34fe35b9b869e3e140f4596ccd416fbe1d07bdbe
Jonathan Lam <jlam55555@gmail.com>
2017-07-15 19:55:12 -0400

    Changes for mobile app resizes

--- res/main.js
+++ res/main.js
@@ -4,12 +4,6 @@ $(function() {
   // src: https://gist.github.com/irae/1042167 (condensed version)
   (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(chref=d.href).replace(e.href,"").indexOf("#")&&(!/^[a-z\+\.\-]+:/i.test(chref)||chref.indexOf(e.protocol+"//"+e.host)===0)&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone");
 
-  // add extra top section on web app
-  if(("standalone" in navigator) && navigator["standalone"]) {
-    $("#content").addClass("webApp");
-    $("#sidebar").addClass("webApp"); 
-  }
-
   // mobile menu
   var menuButton = $("#menuButton");
   var sidebar = $("#sidebar");
@@ -31,8 +25,20 @@ $(function() {
   });
 
   $(window).resize(function() {
-    if($(this).width() > 1024 && sidebar.hasClass("expanded")) {
+    if($(this).width() >= 1024 && sidebar.hasClass("expanded")) {
       sidebar.removeClass("expanded");
     }
-  });
+
+    // add extra top section on web app
+    if($(this).width() < 1024 && ("standalone" in navigator) && navigator["standalone"]) {
+      $("#content").addClass("webApp");
+      $("#sidebar").addClass("webApp"); 
+    }
+    // remove extra top section if width changes to greater than 1024
+    if($("#content").hasClass("webApp") && $(this).width() >= 1024) {
+      $("#content").removeClass("webApp");
+      $("#sidebar").removeClass("webApp");
+    }
+
+  }).resize();
 });
