cbaf420e056a1b640989474036bfe075ebd4209e
Jonathan Lam <jlam55555@gmail.com>
2018-04-26 01:29:20 -0400

    minor debugging

--- server.js
+++ server.js
@@ -192,6 +192,7 @@ fs.readdir("./posts", function(error, posts) {
             .then(function(data) {
               postData.id = data.id;
               postData.comments = JSON.parse(data.comments);
+              console.log(postData.comments);
               postData.hitcount = data.hitcount;
             })
             .catch(function(e) {
